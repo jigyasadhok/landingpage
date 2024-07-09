ad841f3f8112501dd0121b752c51964da3f0a42e
jigyasadhok <jigyasadhok9@gmail.com>
2024-07-09 18:57:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+addEventListener("scroll", function(){
+    document.querySelector("#header").style.backgroundColor = "white";
+    document.querySelector("#header").style.color = "black";
+    
+})
